aefbc427eba763ca1b8fbef23d83ba6a0792b2a5
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-12-13 11:22:16 +0100

    initial commit

---
+++ src/test/java/com/mike/gymmanagement/GymManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.gymmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
