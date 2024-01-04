02cb323014c9b0dc602103d9e4a661f204663d76
TheArcus02 <m.urbaniak.inf@gmail.com>
2024-01-04 21:22:21 +0100

    CORS origin access for localhost

--- src/main/java/com/mike/gymmanagement/controller/EquipmentController.java
+++ src/main/java/com/mike/gymmanagement/controller/EquipmentController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/equipment")
 public class EquipmentController {
